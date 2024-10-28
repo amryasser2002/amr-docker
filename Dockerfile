ee7eb74c1fd9ae87605dd1aa42493bd3c6629a27
amryasser2002 <amryasser445@gmail.com>
2024-10-29 02:32:23 +0300

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM node:16 as base
+
+
+FROM base as production
+
+WORKDIR /app
+COPY package.json .
+RUN npm install --only=production
+COPY . .
+EXPOSE 5002
+CMD ["npm" , "start" ]
+
+FROM node:14 as development
+
+WORKDIR /app
+COPY package.json .
+RUN npm install
+COPY . .
+EXPOSE 5002
+CMD ["npm" , "run" , "start-dev" ]
---
+++ index.js
@@ -0,0 +1,17 @@
+    const express = require('express')
+   // const mongoose = require('mongoose');
+
+    const PORT = 5002;
+    const app = express();
+   // mongoose.connect(`mongodb://root:example@mongo:27017`);
+
+    app.get('/', (req, res) => res.send('<h1> hello amr 555 <h1>'));
+
+    app.listen(PORT, () => console.log(`app is on on port : ${PORT}`));
+   // .then(() => console.log('connect to db .....')).catch((err) => console.log('failed to connect ...:', err))
+   //connect to db
+    /*const DB_USER = 'root';
+    const DB_PASSWORD = 'example';
+    const DB_PORT = 27017;
+    const DB_HOST = 'mongo'
+    const URI = ('mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}');*/
